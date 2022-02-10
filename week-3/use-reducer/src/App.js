01cda87b0bfb25dde98fb32e3c138cd133e52aff
Chrysalis <63181733+chrysaliswoon@users.noreply.github.com>
2022-02-10 19:38:41 +0800

    create react files for useReducer

---
+++ week-3/use-reducer/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React from "react";
+import Counter from './Counter'
+
+function App() {
+  return (
+    <div className="nes-text is-primary">
+      <Counter />
+    </div>
+  );
+}
+
+export default App
