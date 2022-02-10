8db204a795fa885734a50df992aeb5be9e407d9a
Chrysalis <63181733+chrysaliswoon@users.noreply.github.com>
2022-02-10 20:56:29 +0800

    Created placeholder codes for Light Levels

--- week-3/light-levels/src/App.js
+++ week-3/light-levels/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Controls from './Controls'
+import Light from './Light'
+import lightData from './lightData'
+import './App.css'
+console.log('lightData', lightData)
 
 function App() {
+    const controls = ['off', "low", 'medium', 'high']
+    
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Controls controls={controls} />
+      <Light />
     </div>
+
   );
 }
 
-export default App;
+export default App
