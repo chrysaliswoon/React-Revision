8bbacc3ce3a3eb02ca5410dda5ba6f517a2386ca
Chrysalis <63181733+chrysaliswoon@users.noreply.github.com>
2022-02-10 21:06:14 +0800

    Created placeholder codes for shopping-cart

--- week-3/shopping-cart-refactor/src/App.js
+++ week-3/shopping-cart-refactor/src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
+// import useState
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+// import the required components
+import AllTheThings from './AllTheThings';
+import MyShoppingCart from './MyShoppingCart';
+import Form from './Form';
+// import the products array from products.js
+import productsArr from './products';
+
+export default function App() {
+  // add state for products and cart, both of which are empty arrays
+  const [products, setProducts] = useState(productsArr);
+  const [cart, setCart] = useState([]);
+  // create a addToCart function that takes in an item as a param
+  const addToCart = (item) => {
+    setCart([...cart, item]);
+  };
+  // create a removeFromCart function that takes in an index as a param
+  const removeFromCart = (index) => {
+    const cartArr = cart.filter((d, i) => i !== index);
+    setCart(cartArr);
+  };
+
+  const addToProducts = (product) => {
+    setProducts([product, ...products]);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Big Time Shopping</h1>
+      <Form handleSubmit={addToProducts} />
+      <div className="products">
+        <AllTheThings products={products} handleClick={addToCart} />
+        <MyShoppingCart cart={cart} handleClick={removeFromCart} />
+      </div>
     </div>
   );
 }
-
-export default App;
