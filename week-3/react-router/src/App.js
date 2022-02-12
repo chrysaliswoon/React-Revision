8bd3cd1f8c93362ebc2edecd2c40fba7e707bda3
Chrysalis <63181733+chrysaliswoon@users.noreply.github.com>
2022-02-12 11:20:50 +0800

    Created Index Routes

--- week-3/react-router/src/App.js
+++ week-3/react-router/src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import {Outlet, Link} from "react-router-dom" // Render an Outlet
 
 function App() {
   return (
     <div className="App">
-
+      <h1>Bookkeeper!</h1>
+      <nav style={{
+        borderBottom: "solid 1px",
+        paddingBottom: "1rem"
+      }}
+      >
+        <Link to="./invoices">Invoices</Link> |{" "}
+        <Link to="./expenses">Expenses</Link>
+      </nav>
+      <Outlet />
     </div>
   );
 }
