e6f88079b2e74c8ee409e70388b87eb5c69f0993
Chrysalis <63181733+chrysaliswoon@users.noreply.github.com>
2022-02-17 01:37:51 +0800

    Completed Complex State

--- react-intro/state-event/src/App.js
+++ react-intro/state-event/src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
+import {useState} from 'react'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// function App() {
+//   const [left, setLeft] = useState(0)
+//   const [right, setRight] = useState(0)
 
-export default App;
+//   return (
+//     <div className="App">
+//       {left}
+//       <button onClick={() => setLeft(left + 1)}> Left </button>
+//       <button onClick={() => setRight(right + 1)}> Right </button>
+//       {right}
+//     </div>
+//   );
+// }
+
+// export default App;
+
+export default function App() {
+  const [clicks, setClicks] = useState({
+    left: 0, right: 0
+  })
+
+  // const handleLeftClick = () => {
+  //   const newClicks = {
+  //     ...clicks,
+  //     left: clicks.left + 1,
+  //     // right: clicks.right
+  //   }
+  //   setClicks(newClicks)
+  // }
+
+  // const handleRightClick = () => {
+  //   const newClicks = {
+  //     // left: clicks.left,
+  //     ...clicks,
+  //     right: clicks.right + 1
+  //   }
+  //   setClicks(newClicks)
+  // }
+
+  const handleLeftClick = () =>
+  setClicks({...clicks, left: clicks.left + 1})
+
+  const handleRightClick = () =>
+  setClicks({...clicks, right: clicks.right + 1})
+
+  return (
+    <div className = "App">
+      {clicks.left}
+      <button onClick={handleLeftClick}>Left</button>
+      <button onClick={handleRightClick}>Right</button>
+      {clicks.right}
+    </div>
+  )
+}
