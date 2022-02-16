3e7c0a8045d8ee731c357be1616519e87f8f3600
Chrysalis <63181733+chrysaliswoon@users.noreply.github.com>
2022-02-17 01:26:05 +0800

    Render images for React City

--- week-1/react-cities/src/App.js
+++ week-1/react-cities/src/App.js
@@ -2,12 +2,16 @@ import './App.css';
 import CityImage from './image'
 
 function App() {
+  
+  const images = CityImage.map((image => (
+    <img className="thumb" src={image.img} alt={image.city}/>
+  ))) 
 
   return (
     <div className="App">
       <h1>React Cities</h1>
+      <div id="thumbnails">{images}</div>
       </div>
-    </div>
   );
 }
 
