486f45c32b79dede890564d4bed77d1ef5d32f68
Chrysalis <63181733+chrysaliswoon@users.noreply.github.com>
2022-02-19 14:04:19 +0800

    Create files for array-testing

---
+++ week-4/array-testing/src/Algos/flatten.js
@@ -0,0 +1,11 @@
+export default function flatten(arr, result = []) {
+    arr.forEach(elm => {
+        switch(true) {
+            case Array.isArray(elm) :
+                flatten(elm, result); break 
+            default :
+                result.push(elm)
+        }
+    })
+    return result
+}
