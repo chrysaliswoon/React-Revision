8b463063aa0611eaf24f671d376660425203db38
Chrysalis <63181733+chrysaliswoon@users.noreply.github.com>
2022-02-19 14:16:25 +0800

    created files for array-testing

--- week-4/array-testing/src/App.js
+++ week-4/array-testing/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-
+      <h1>Flatten An Array Test</h1>
     </div>
   );
 }
---
+++ week-4/array-testing/src/Component/Counter.js
@@ -0,0 +1,20 @@
+import React, {useState} from 'react'
+
+export default function Counter (props) {
+    const [count, setCount] = useState(0)
+
+    return (
+        <div>
+            <h1>Counter</h1>
+            <div>
+                Current Count:
+                <span className="counter">{counter}</span>
+            </div>
+
+            <section>
+                <button className="plus" onClick={() => setCount(count + 1)}> + </button>
+                <button className="minus" onClick={() => setCount(count - 1)}> - </button>
+            </section>
+        </div>
+    )
+}
